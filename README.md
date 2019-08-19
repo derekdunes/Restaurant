b919bd6dd9e42d3b26a3a83d80cbc17c20f7c84f
DEREKDUNES <mbahderek@gmail.com>
2019-08-19 16:09:56 +0100

    The first commit pn RestaurAnt

---
+++ README.md
@@ -0,0 +1,1 @@
+#RestaurAnt
---
+++ js/main.js
@@ -0,0 +1,7 @@
+var mainNav = document.getElementById('js-menu');
+
+var navBarToggle = document.getElementById('js-navbar-toggle');
+
+navBarToggle.addEventListener('click', function() {
+	mainNav.classList.toggle('active');
+})
